c6fdfeebbd6a88cc08e718e69c965110dc2c216f
Malekal <78604153+Malekal4699@users.noreply.github.com>
2022-01-22 17:30:06 -0500

    fixed item consumption

--- scripts/LightDataExt.js
+++ scripts/LightDataExt.js
@@ -1,5 +1,6 @@
 export class LightDataExt extends foundry.data.LightData {
   name;
+  tokenID;
   type;
   state;
 
@@ -8,6 +9,7 @@ export class LightDataExt extends foundry.data.LightData {
     this.name = name ?? "SampleName";
     this.type = type ?? "LightType";
     this.state = state ?? "false";
+    this.tokenID = app.object.id;
     this._initFlag(app);
   };
 
@@ -20,4 +22,24 @@ export class LightDataExt extends foundry.data.LightData {
     return this.name + this.type;
   };
 
+  async turnOff() {
+    this.state = false;
+    await game.actor.setFlag("LightsHUD", _getFlagName(), false);
+    tbuttonLantern.removeClass("active");
+    // Lantern is inactive, enable the relevant light sources according to parameters
+    enableRelevantButtons();
+    // Restore the initial light source
+    updateTokenLighting(
+      tokenD.getFlag("LightsHUD", "InitialBrightRadius"),
+      tokenD.getFlag("LightsHUD", "InitialDimRadius"),
+      tokenD.getFlag("LightsHUD", "InitialLightColor"),
+      tokenD.getFlag("LightsHUD", "InitialColorIntensity"),
+      tokenD.getFlag("LightsHUD", "Initiallight.angle"),
+      tokenD.getFlag("LightsHUD", "InitialAnimationType"),
+      tokenD.getFlag("LightsHUD", "InitialAnimationSpeed"),
+      tokenD.getFlag("LightsHUD", "InitialAnimationIntensity")
+    );
+  }
 }
+
+
